a3df344b5c11ac9549d6d2c85b04a14b9e204cb1
Nikita Juyal <32689770+juyalnikita@users.noreply.github.com>
2017-12-15 23:40:16 +0000

    Intialise fileServer

---
+++ runFileServer.py
@@ -0,0 +1,13 @@
+import web
+
+import dfs.fileserver
+
+urls = (
+        '(/.*)', 'dfs.fileserver.FileServer',
+       )
+
+app = web.application(urls, globals())
+
+
+if __name__ == '__main__':
+    app.run()
